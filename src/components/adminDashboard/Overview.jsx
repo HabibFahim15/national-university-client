ba59a1ec8caa2e9ad4045c3a2ffa26c392dfcd3a
HabibHossain <habibfahim71@gmail.com>
2025-04-30 20:28:20 +0600

    overview design are done

--- src/components/adminDashboard/Overview.jsx
+++ src/components/adminDashboard/Overview.jsx
@@ -1,5 +1,6 @@
 import { FaUsers } from "react-icons/fa6";
-import { FaGraduationCap } from "react-icons/fa6";
+import { FaGraduationCap,FaRegMoneyBill1 } from "react-icons/fa6";
+import { RiParentFill } from "react-icons/ri";
 const Overview = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
@@ -25,53 +26,25 @@ const Overview = () => {
         </div>
         <FaGraduationCap className="text-6xl text-[#1e33f2]" />
       </div>
-      <div className="rounded-2xl items-center border py-14 px-12 text-start shadow-lg bg-[#ffffff] gap-8 flex justify-start">
-        <div className="relative flex items-center justify-center">
-          <svg
-            width="100"
-            height="100"
-            viewBox="0 0 100 100"
-            className="transform  -rotate-90"
-          >
-            {/* Background Circle */}
-            <circle
-              cx="50"
-              cy="50"
-              r="46"
-              stroke="#e5e7eb"
-              strokeWidth="12"
-              fill="none"
-              className="dark:stroke-gray-700" // Added dark mode support
-            />
-            {/* Progress Circle */}
-            <circle
-              cx="50"
-              cy="50"
-              r="46"
-              stroke="currentColor"
-              strokeWidth="12"
-              fill="none"
-              strokeDasharray="250"
-              strokeDashoffset="58"
-              strokeLinecap="round"
-              className="text-red-500 dark:text-red-400 transition-all duration-500"
-            />
-          </svg>
-          <span className="absolute text-black dark:text-white font-bold text-sm">
-            65%
-          </span>
-        </div>
-
+      <div className="rounded-2xl items-center border py-14 px-12 text-start shadow-lg bg-[#ffffff] flex justify-between">
         <div>
-          <h1 className="text-4xl mb-3 font-bold">886</h1>
-          <h3 className="text-xl font-semibold">Events</h3>
+        <h4 className="text-xl text-gray-600 font-semibold">Total Parents</h4>
+        <h1 className="text-4xl font-semibold">4398</h1>
+          
         </div>
-
-        {/* ToDo: there will be a small rounded progress bar and event count */}
+        <RiParentFill className="text-6xl text-red-600" />
       </div>
       <div className="rounded-2xl items-center border py-14 px-12 text-start shadow-lg bg-[#ffffff] flex justify-between">
-        {/* ToDo: there will be a small rounded progress bar and Food count */}
+        <div>
+        <h4 className="text-xl text-gray-600 font-semibold">Earning</h4>
+          <h1 className="text-3xl font-semibold">$195482</h1>
+          
+         
+        </div>
+        <FaRegMoneyBill1 className="text-6xl text-green-600" />
       </div>
+      
+      
     </div>
   );
 };
